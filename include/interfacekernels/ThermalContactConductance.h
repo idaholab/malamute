4a2b225f362320eee4fc2de94e497f711118e51a
Icenhour, Casey Tyler <casey.icenhour@inl.gov>
2020-06-02 15:37:46 -0600

    Address review comments (#10)
    Shorten Cincotti bibliography entry
    
    Adjust name conventions in ThermalContactResistance.C and add user-adjustable splitting factor for joule heating.
    
    Adjust header variable names, and add doxygen documentation
    
    Update website documentation for ThermalContactConductance
    
    Add Jacobian comments for ThermalContactConductance.C
    
    Adjust thermal_interface test and update test spec file with SQA requirements, design, and related issues.

--- include/interfacekernels/ThermalContactConductance.h
+++ include/interfacekernels/ThermalContactConductance.h
@@ -2,6 +2,12 @@
 
 #include "InterfaceKernel.h"
 
+/**
+ * This interfacekernel implements the thermal contact conductance across a
+ * boundary formed between two dissimilar materials (resulting in a temperature
+ * discontinuity) under the influence of an electrostatic potential.
+ */
+
 class ThermalContactConductance : public InterfaceKernel
 {
 public:
@@ -14,16 +20,36 @@ protected:
   virtual Real computeQpJacobian(Moose::DGJacobianType type) override;
   virtual Real computeQpOffDiagJacobian(Moose::DGJacobianType type, unsigned int jvar) override;
 
-  const Real & _htc;
-  const Real & _electrical_contact_resistance;
+  /// Thermal contact conductance coefficient (indicates ability to conduct heat across interface)
+  const Real & _thermal_contact_conductance;
+
+  /// Electrical contact conductance coefficient (indicates ability to conduct heat as a result of electrostatic joule heating across interface)
+  const Real & _electrical_contact_conductance;
+
+  /// The electrostatic potential value associated with the master side of the interface
   const VariableValue & _potential_master;
+
+  /// The electrostatic potential value associated with the neighbor side of the interface
   const VariableValue & _potential_neighbor;
 
-  // For computeQpOffDiagJacobian
+  /// Variable ID for the master electrostatic potential variable
   unsigned int _potential_master_id;
+
+  /// Master electrostatic potential variable
   MooseVariable & _potential_master_var;
+
+  /// Master electrostatic potential variable basis function
   const VariablePhiValue & _potential_master_phi;
+
+  /// Variable ID for the neighbor electrostatic potential variable
   unsigned int _potential_neighbor_id;
+
+  /// Neighbor electrostatic potential variable
   MooseVariable &  _potential_neighbor_var;
+
+  /// Neighbor electrostatic potential variable basis function
   const VariablePhiValue & _potential_neighbor_phi;
+
+  /// Splitting factor for joule heating source between master and neighbor sides
+  const Real & _splitting_factor;
 };
--- src/interfacekernels/ThermalContactConductance.C
+++ src/interfacekernels/ThermalContactConductance.C
@@ -16,6 +16,7 @@ ThermalContactConductance::validParams()
   params.addRequiredCoupledVar("master_potential", "Electrostatic potential on the master block.");
   params.addRequiredCoupledVar("neighbor_potential",
                                "Electrostatic potential on the neighbor block.");
+  params.addParam<Real>("splitting_factor", 0.5, "Splitting factor of the Joule heating source.");
   params.addClassDescription(
       "Interface condition that describes the thermal contact resistance across a boundary formed "
       "between two dissimilar materials (resulting in a temperature discontinuity) under the "
@@ -27,8 +28,8 @@ ThermalContactConductance::validParams()
 
 ThermalContactConductance::ThermalContactConductance(const InputParameters & parameters)
   : InterfaceKernel(parameters),
-    _htc(getParam<Real>("htc")),
-    _electrical_contact_resistance(getParam<Real>("electrical_contact_resistance")),
+    _thermal_contact_conductance(getParam<Real>("thermal_contact_conductance")),
+    _electrical_contact_conductance(getParam<Real>("electrical_contact_conductance")),
     _potential_master(coupledValue("master_potential")),
     _potential_neighbor(coupledValue("neighbor_potential")),
     _potential_master_id(coupled("master_potential")),
@@ -36,26 +37,26 @@ ThermalContactConductance::ThermalContactConductance(const InputParameters & par
     _potential_master_phi(_assembly.phiFace(_potential_master_var)),
     _potential_neighbor_id(coupled("neighbor_potential")),
     _potential_neighbor_var(*getVar("neighbor_potential", 0)),
-    _potential_neighbor_phi(_assembly.phiFaceNeighbor(_potential_neighbor_var))
+    _potential_neighbor_phi(_assembly.phiFaceNeighbor(_potential_neighbor_var)),
+    _splitting_factor(getParam<Real>("splitting_factor"))
 {
 }
 
 Real
 ThermalContactConductance::computeQpResidual(Moose::DGResidualType type)
 {
-  Real q_electric = 0.5 * _electrical_contact_resistance *
-                    (_potential_master[_qp] - _potential_neighbor[_qp]) *
-                    (_potential_master[_qp] - _potential_neighbor[_qp]);
+  Real q_electric = _electrical_contact_conductance *
+                    std::pow((_potential_master[_qp] - _potential_neighbor[_qp]), 2);
 
-  Real q_temperature = _htc * (_u[_qp] - _neighbor_value[_qp]);
+  Real q_temperature = _thermal_contact_conductance * (_u[_qp] - _neighbor_value[_qp]);
 
   switch (type)
   {
     case Moose::Element:
-      return (q_temperature - q_electric) * _test[_i][_qp];
+      return (q_temperature - _splitting_factor * q_electric) * _test[_i][_qp];
 
     case Moose::Neighbor:
-      return -(q_temperature + q_electric) * _test_neighbor[_i][_qp];
+      return -(q_temperature + (1 - _splitting_factor) * q_electric) * _test_neighbor[_i][_qp];
 
     default:
       return 0.0;
@@ -67,17 +68,21 @@ ThermalContactConductance::computeQpJacobian(Moose::DGJacobianType type)
 {
   switch (type)
   {
+    // element residual statement, element (master) temperature variable is variable of differentiation
     case Moose::ElementElement:
-      return _htc * _phi[_j][_qp] * _test[_i][_qp];
+      return _thermal_contact_conductance * _phi[_j][_qp] * _test[_i][_qp];
 
+    // neighbor residual statement, neighbor temperature variable is variable of differentiation
     case Moose::NeighborNeighbor:
-      return _htc * _phi_neighbor[_j][_qp] * _test_neighbor[_i][_qp];
+      return _thermal_contact_conductance * _phi_neighbor[_j][_qp] * _test_neighbor[_i][_qp];
 
+    // neighbor residual statement, element (master) temperature variable is variable of differentiation
     case Moose::NeighborElement:
-      return _htc * -_phi[_j][_qp] * _test_neighbor[_i][_qp];
+      return _thermal_contact_conductance * -_phi[_j][_qp] * _test_neighbor[_i][_qp];
 
+    // element residual statement, neighbor temperature variable is variable of differentiation
     case Moose::ElementNeighbor:
-      return _htc * -_phi_neighbor[_j][_qp] * _test[_i][_qp];
+      return _thermal_contact_conductance * -_phi_neighbor[_j][_qp] * _test[_i][_qp];
 
     default:
       return 0.0;
@@ -89,33 +94,37 @@ ThermalContactConductance::computeQpOffDiagJacobian(Moose::DGJacobianType type, 
 {
   switch (type)
   {
+    // element residual statement, element (master) potential variable is variable of differentiation
     case Moose::ElementElement:
       if (jvar == _potential_master_id)
-        return -_electrical_contact_resistance *
+        return -2 * _splitting_factor * _electrical_contact_conductance *
                (_potential_master[_qp] - _potential_neighbor[_qp]) *
                _potential_master_phi[_j][_qp] * _test[_i][_qp];
       else
         return 0.0;
 
+    // neighbor residual statement, neighbor potential variable is variable of differentiation
     case Moose::NeighborNeighbor:
       if (jvar == _potential_neighbor_id)
-        return -_electrical_contact_resistance *
+        return -2 * (1 - _splitting_factor) * _electrical_contact_conductance *
                (_potential_master[_qp] - _potential_neighbor[_qp]) *
                -_potential_neighbor_phi[_j][_qp] * _test_neighbor[_i][_qp];
       else
         return 0.0;
 
+    // neighbor residual statement, element (master) potential variable is variable of differentiation
     case Moose::NeighborElement:
       if (jvar == _potential_master_id)
-        return -_electrical_contact_resistance *
+        return -2 * (1 - _splitting_factor) * _electrical_contact_conductance *
                (_potential_master[_qp] - _potential_neighbor[_qp]) *
                _potential_master_phi[_j][_qp] * _test_neighbor[_i][_qp];
       else
         return 0.0;
 
+    // element residual statement, neighbor potential variable is variable of differentiation
     case Moose::ElementNeighbor:
       if (jvar == _potential_neighbor_id)
-        return -_electrical_contact_resistance *
+        return -2 * _splitting_factor * _electrical_contact_conductance *
                (_potential_master[_qp] - _potential_neighbor[_qp]) *
                -_potential_neighbor_phi[_j][_qp] * _test[_i][_qp];
       else
