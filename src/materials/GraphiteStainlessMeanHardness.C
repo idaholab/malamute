a12cf888ee1829b0f38afcdced78319c889d4310
Icenhour, Casey Tyler <casey.icenhour@inl.gov>
2020-06-16 12:36:40 -0600

    Add harmonic mean of graphite and stainless steel hardness
    Refs #13

---
+++ src/materials/GraphiteStainlessMeanHardness.C
@@ -0,0 +1,27 @@
+#include "GraphiteStainlessMeanHardness.h"
+
+registerMooseObject("FreyaApp", GraphiteStainlessMeanHardness);
+
+InputParameters
+GraphiteStainlessMeanHardness::validParams()
+{
+  InputParameters params = ADMaterial::validParams();
+  params.addClassDescription(
+      "Calculates the harmonic mean of the hardness values of graphite and stainless steel.");
+  return params;
+}
+
+GraphiteStainlessMeanHardness::GraphiteStainlessMeanHardness(const InputParameters & parameters)
+  : Material(parameters),
+    _graphite_hardness(3.5e9),
+    _stainless_steel_hardness(1.92e9),
+    _mean_hardness(declareADProperty<Real>("graphite_stainless_mean_hardness"))
+{
+}
+
+void
+GraphiteStainlessMeanHardness::computeQpProperties()
+{
+  _mean_hardness[_qp] = 2. * _graphite_hardness * _stainless_steel_hardness /
+                        (_graphite_hardness + _stainless_steel_hardness);
+}
