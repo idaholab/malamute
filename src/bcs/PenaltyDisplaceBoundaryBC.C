0297315b83bed0dc62de67374db41ca7c241e2ff
Icenhour, Casey Tyler <casey.icenhour@inl.gov>
2021-12-23 14:37:23 -0700

    Add malamute header to former baldr source (#5)

--- src/bcs/PenaltyDisplaceBoundaryBC.C
+++ src/bcs/PenaltyDisplaceBoundaryBC.C
@@ -1,3 +1,12 @@
+/****************************************************************************/
+/*                        DO NOT MODIFY THIS HEADER                         */
+/*                                                                          */
+/* MALAMUTE: MOOSE Application Library for Advanced Manufacturing UTilitiEs */
+/*                                                                          */
+/*               Copyright 2021, Battelle Energy Alliance, LLC              */
+/*                           ALL RIGHTS RESERVED                            */
+/****************************************************************************/
+
 #include "PenaltyDisplaceBoundaryBC.h"
 
 registerMooseObject("MalamuteApp", PenaltyDisplaceBoundaryBC);
