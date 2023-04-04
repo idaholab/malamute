7c8d863d52ac354fca38f7db6785ba0dbcb1f1fc
dewenyushu <dewen.yushu@inl.gov>
2023-04-04 14:43:37 -0700

    Add velocity gaussian heat source and tests for effective radii calculation using experimental measurements

---
+++ include/materials/VelocityGaussianHeatSource.h
@@ -0,0 +1,44 @@
+/****************************************************************************/
+/*                        DO NOT MODIFY THIS HEADER                         */
+/*                                                                          */
+/* MALAMUTE: MOOSE Application Library for Advanced Manufacturing UTilitiEs */
+/*                                                                          */
+/*           Copyright 2021 - 2023, Battelle Energy Alliance, LLC           */
+/*                           ALL RIGHTS RESERVED                            */
+/****************************************************************************/
+
+#pragma once
+
+#include "GaussianHeatSourceBase.h"
+
+
+/**
+ * Double ellipsoid heat source distribution.
+ */
+class VelocityGaussianHeatSource : public GaussianHeatSourceBase
+{
+public:
+  static InputParameters validParams();
+
+  VelocityGaussianHeatSource(const InputParameters & parameters);
+
+protected:
+  virtual void
+  computeHeatSourceCenterAtTime(Real & x, Real & y, Real & z, const Real & time) override;
+
+  virtual void computeHeatSourceMovingSpeedAtTime(const Real & time) override;
+
+  /// previous time
+  Real _prev_time;
+
+  /// position at previous time
+  Real _x_prev, _y_prev, _z_prev;
+
+  /// function of scanning speed along three directions
+  const Function & _function_vx;
+  const Function & _function_vy;
+  const Function & _function_vz;
+
+  /// stores the velocity at the current step
+  Real _vx, _vy, _vz;
+};
