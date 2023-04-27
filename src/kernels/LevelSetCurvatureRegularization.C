0470c9fe5892e39a3a55ba95417a1b99494a281c
Alex Lindsay <alexlindsay239@gmail.com>
2023-04-27 16:04:35 -0700

    Remove singularity from calling norm on a zero vector

--- src/kernels/LevelSetCurvatureRegularization.C
+++ src/kernels/LevelSetCurvatureRegularization.C
@@ -33,9 +33,10 @@ LevelSetCurvatureRegularization::LevelSetCurvatureRegularization(const InputPara
 ADReal
 LevelSetCurvatureRegularization::computeQpResidual()
 {
-  if (MetaPhysicL::raw_value(_grad_c[_qp].norm()) > libMesh::TOLERANCE)
+  const auto grad_c_norm = MooseUtils::isZero(_grad_c[_qp]) ? ADReal(1e-42) : _grad_c[_qp].norm();
+  if (MetaPhysicL::raw_value(grad_c_norm) > libMesh::TOLERANCE)
   {
-    ADRealVectorValue n = (_grad_c[_qp]) / _grad_c[_qp].norm();
+    ADRealVectorValue n = (_grad_c[_qp]) / grad_c_norm;
     return _test[_i][_qp] * _u[_qp] - _grad_test[_i][_qp] * (n - _varepsilon * _grad_u[_qp]);
   }
   else
